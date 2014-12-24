25d0cfb7f721fe14c11f0347739a1df9f004b607
François Dupayrat <fradow@gmail.com>
2014-12-24 14:52:48 +0100

    Add cocos_ui and spine in project, disable chipmunk

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -21,16 +21,18 @@ HEADER_FILES := $(shell find $(LOCAL_PATH)/../../Classes -type d)
 LOCAL_C_INCLUDES := $(HEADER_FILES:$(LOCAL_PATH)/%=%)
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos_ui_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dxandroid_static
 
 # LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosbuilder_static
-# LOCAL_WHOLE_STATIC_LIBRARIES += spine_static
+LOCAL_WHOLE_STATIC_LIBRARIES += spine_static
 # LOCAL_WHOLE_STATIC_LIBRARIES += cocostudio_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_network_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
 
+LOCAL_CXXFLAGS += -fexceptions
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -39,7 +41,7 @@ $(call import-module,audio/android)
 
 # $(call import-module,Box2D)
 $(call import-module,editor-support/cocosbuilder)
-# $(call import-module,editor-support/spine)
+$(call import-module,editor-support/spine)
 # $(call import-module,editor-support/cocostudio)
 $(call import-module,network)
 $(call import-module,extensions)
--- proj.android/jni/Application.mk
+++ proj.android/jni/Application.mk
@@ -1,7 +1,7 @@
 APP_STL := c++_static
 NDK_TOOLCHAIN_VERSION=clang
 
-APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
+APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=0 -std=c++11 -fsigned-char
 APP_LDFLAGS := -latomic
 
 APP_ABI := armeabi armeabi-v7a x86
