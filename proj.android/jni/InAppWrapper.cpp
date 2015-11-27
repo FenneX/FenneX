df19ae02a9711356d70e083a60103687603decb2
François Dupayrat <fradow@gmail.com>
2015-11-27 15:16:07 +0100

    Fix JNI GetMethods to be outside of CCAsserts, fix indentation

--- proj.android/jni/InAppWrapper.cpp
+++ proj.android/jni/InAppWrapper.cpp
@@ -1,26 +1,26 @@
 /****************************************************************************
-Copyright (c) 2013-2014 Auticiel SAS
-
-http://www.fennex.org
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************///
+ Copyright (c) 2013-2014 Auticiel SAS
+ 
+ http://www.fennex.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************///
 
 #include "FenneX.h"
 #include <jni.h>
@@ -41,61 +41,66 @@ THE SOFTWARE.
 void initializePayements()
 {
     LOGD("Getting initialize payements method info");
-	JniMethodInfo minfo;
-	CCAssert(JniHelper::getStaticMethodInfo(minfo,CLASS_NAME,"initialize", "()V"), "Function doesn't exist");
-	if(minfo.env->ExceptionCheck())
-	{
-		LOGD("Problem with initialize payements:");
-		minfo.env->ExceptionDescribe();
-	}
+    JniMethodInfo minfo;
+    bool functionExist = JniHelper::getStaticMethodInfo(minfo,CLASS_NAME,"initialize", "()V");
+    CCAssert(functionExist, "Function doesn't exist");
+    if(minfo.env->ExceptionCheck())
+    {
+        LOGD("Problem with initialize payements:");
+        minfo.env->ExceptionDescribe();
+    }
     LOGD("initializing payements ...");
-	minfo.env->CallStaticVoidMethod(minfo.classID, minfo.methodID);
-	minfo.env->DeleteLocalRef(minfo.classID);
+    minfo.env->CallStaticVoidMethod(minfo.classID, minfo.methodID);
+    minfo.env->DeleteLocalRef(minfo.classID);
     LOGD("payements initialized!");
-	if(minfo.env->ExceptionCheck())
-	{
-		LOGD("Problem with initialize payements (after close):");
-		minfo.env->ExceptionDescribe();
-	}
+    if(minfo.env->ExceptionCheck())
+    {
+        LOGD("Problem with initialize payements (after close):");
+        minfo.env->ExceptionDescribe();
+    }
 }
 
 void inAppPurchaseProduct(const std::string& productID)
 {
-	JniMethodInfo minfo;
-	CCAssert(JniHelper::getStaticMethodInfo(minfo,CLASS_NAME,"buyProductIdentifier", "(Ljava/lang/String;)V"), "Function doesn't exist");
+    JniMethodInfo minfo;
+    bool functionExist = JniHelper::getStaticMethodInfo(minfo,CLASS_NAME,"buyProductIdentifier", "(Ljava/lang/String;)V");
+    CCAssert(functionExist, "Function doesn't exist");
     jstring jproductID = minfo.env->NewStringUTF(productID.c_str());
-	minfo.env->CallStaticVoidMethod(minfo.classID, minfo.methodID, jproductID);
-	minfo.env->DeleteLocalRef(minfo.classID);
+    minfo.env->CallStaticVoidMethod(minfo.classID, minfo.methodID, jproductID);
+    minfo.env->DeleteLocalRef(minfo.classID);
     minfo.env->DeleteLocalRef(jproductID);
 }
 
 void restoreTransaction(const std::string& productID)
 {
-	JniMethodInfo minfo;
-	CCAssert(JniHelper::getStaticMethodInfo(minfo,CLASS_NAME,"restoreTransaction", "(Ljava/lang/String;)V"), "Function doesn't exist");
+    JniMethodInfo minfo;
+    bool functionExist = JniHelper::getStaticMethodInfo(minfo,CLASS_NAME,"restoreTransaction", "(Ljava/lang/String;)V");
+    CCAssert(functionExist, "Function doesn't exist");
     jstring jproductID = minfo.env->NewStringUTF(productID.c_str());
     LOGD("calling restore transactions ...");
-	minfo.env->CallStaticVoidMethod(minfo.classID, minfo.methodID, jproductID);
-	minfo.env->DeleteLocalRef(minfo.classID);
+    minfo.env->CallStaticVoidMethod(minfo.classID, minfo.methodID, jproductID);
+    minfo.env->DeleteLocalRef(minfo.classID);
     minfo.env->DeleteLocalRef(jproductID);
     LOGD("done restoring!");
 }
 
 void releasePayements()
 {
-	JniMethodInfo minfo;
-	CCAssert(JniHelper::getStaticMethodInfo(minfo,CLASS_NAME,"release", "()V"), "Function doesn't exist");
-	minfo.env->CallStaticBooleanMethod(minfo.classID, minfo.methodID);
-	minfo.env->DeleteLocalRef(minfo.classID);
+    JniMethodInfo minfo;
+    bool functionExist = JniHelper::getStaticMethodInfo(minfo,CLASS_NAME,"release", "()V");
+    CCAssert(functionExist, "Function doesn't exist");
+    minfo.env->CallStaticBooleanMethod(minfo.classID, minfo.methodID);
+    minfo.env->DeleteLocalRef(minfo.classID);
 }
 
 void requestProductsData(CCArray* products)
 {
-	JniMethodInfo minfo;
-	CCAssert(JniHelper::getStaticMethodInfo(minfo,CLASS_NAME,"requestProductsData", "([Ljava/lang/String;)V"), "Function doesn't exist");
+    JniMethodInfo minfo;
+    bool functionExist = JniHelper::getStaticMethodInfo(minfo,CLASS_NAME,"requestProductsData", "([Ljava/lang/String;)V");
+    CCAssert(functionExist, "Function doesn't exist");
     jobjectArray array = jobjectArrayFromCCArray(minfo.env, products);
-	minfo.env->CallStaticVoidMethod(minfo.classID, minfo.methodID, array);
-	minfo.env->DeleteLocalRef(minfo.classID);
+    minfo.env->CallStaticVoidMethod(minfo.classID, minfo.methodID, array);
+    minfo.env->DeleteLocalRef(minfo.classID);
     minfo.env->DeleteLocalRef(array);
 }
 
@@ -104,43 +109,45 @@ CCDictionary* productsInfos = NULL;
 
 CCDictionary* getProductsInfos()
 {
-	JniMethodInfo minfo, minfo2;
-	CCAssert(JniHelper::getStaticMethodInfo(minfo,CLASS_NAME,"getProductsIds", "()[Ljava/lang/String;"), "Function doesn't exist");
-	CCLOG("Starting getProductsInfos ...");
-	jobjectArray productsIdNative = (jobjectArray)minfo.env->CallStaticObjectMethod(minfo.classID, minfo.methodID);
-	minfo.env->DeleteLocalRef(minfo.classID);
-	CCArray* productsId = CCArrayFromjobjectArray(minfo.env, productsIdNative);
-	minfo.env->DeleteLocalRef(productsIdNative);
-	if(productsInfos == NULL)
-	{
-		productsInfos = new CCDictionary();
-	}
-	if(productsId == NULL)
-	{
-		return productsInfos;
-	}
-
-	CCAssert(JniHelper::getStaticMethodInfo(minfo2,CLASS_NAME,"getProductsInfos", "(Ljava/lang/String;)[Ljava/lang/String;"), "Function doesn't exist");
-
-	for(int i = 0; i < productsId->count(); i++)
-	{
-		CCString* productId = (CCString*)productsId->objectAtIndex(i);
-		if(!isKindOfClass(productsId->objectAtIndex(i), CCString))
-		{
-			CCLOG("Warning : wrong type of product Id at index %d, will crash", i);
-		}
-		CCLOG("getting product \"%s\" infos", productId->getCString());
+    JniMethodInfo minfo, minfo2;
+    bool functionExist = JniHelper::getStaticMethodInfo(minfo,CLASS_NAME,"getProductsIds", "()[Ljava/lang/String;");
+    CCAssert(functionExist, "Function doesn't exist");
+    CCLOG("Starting getProductsInfos ...");
+    jobjectArray productsIdNative = (jobjectArray)minfo.env->CallStaticObjectMethod(minfo.classID, minfo.methodID);
+    minfo.env->DeleteLocalRef(minfo.classID);
+    CCArray* productsId = CCArrayFromjobjectArray(minfo.env, productsIdNative);
+    minfo.env->DeleteLocalRef(productsIdNative);
+    if(productsInfos == NULL)
+    {
+        productsInfos = new CCDictionary();
+    }
+    if(productsId == NULL)
+    {
+        return productsInfos;
+    }
+    
+    functionExist = JniHelper::getStaticMethodInfo(minfo2,CLASS_NAME,"getProductsInfos", "(Ljava/lang/String;)[Ljava/lang/String;");
+    CCAssert(functionExist, "Function doesn't exist");
+    
+    for(int i = 0; i < productsId->count(); i++)
+    {
+        CCString* productId = (CCString*)productsId->objectAtIndex(i);
+        if(!isKindOfClass(productsId->objectAtIndex(i), CCString))
+        {
+            CCLOG("Warning : wrong type of product Id at index %d, will crash", i);
+        }
+        CCLOG("getting product \"%s\" infos", productId->getCString());
         jstring jproductID = minfo2.env->NewStringUTF(productId->getCString());
-		jobjectArray nativeArray = (jobjectArray)minfo2.env->CallStaticObjectMethod(minfo2.classID, minfo2.methodID, jproductID);
+        jobjectArray nativeArray = (jobjectArray)minfo2.env->CallStaticObjectMethod(minfo2.classID, minfo2.methodID, jproductID);
         minfo2.env->DeleteLocalRef(jproductID);
-
-		CCDictionary* infos = CCDictionaryFromjobjectArray(minfo.env, nativeArray);
-		minfo2.env->DeleteLocalRef(nativeArray);
-		productsInfos->setObject(infos, productId->getCString());
-	}
-	minfo2.env->DeleteLocalRef(minfo2.classID);
-	CCLOG("Returning product infos successfully");
-	return productsInfos;
+        
+        CCDictionary* infos = CCDictionaryFromjobjectArray(minfo.env, nativeArray);
+        minfo2.env->DeleteLocalRef(nativeArray);
+        productsInfos->setObject(infos, productId->getCString());
+    }
+    minfo2.env->DeleteLocalRef(minfo2.classID);
+    CCLOG("Returning product infos successfully");
+    return productsInfos;
 }
 
 void notifyInAppEventNative(std::string name, std::string argument)
@@ -157,12 +164,12 @@ void notifyLicenseStatusNative(bool authorized)
 
 extern "C"
 {
-	void Java_com_fennex_modules_InAppManager_notifyInAppEvent(JNIEnv* envParam, jobject thiz, jstring event, jstring argument)
-	{
-		notifyInAppEventNative(JniHelper::jstring2string(event), JniHelper::jstring2string(argument));
-	}
-	void Java_com_fennex_licensing_LicenseInspector_notifyLicenseStatus(JNIEnv* envParam, jobject thiz, jboolean authorized)
-	{
-		notifyLicenseStatusNative((bool)authorized);
-	}
+    void Java_com_fennex_modules_InAppManager_notifyInAppEvent(JNIEnv* envParam, jobject thiz, jstring event, jstring argument)
+    {
+        notifyInAppEventNative(JniHelper::jstring2string(event), JniHelper::jstring2string(argument));
+    }
+    void Java_com_fennex_licensing_LicenseInspector_notifyLicenseStatus(JNIEnv* envParam, jobject thiz, jboolean authorized)
+    {
+        notifyLicenseStatusNative((bool)authorized);
+    }
 }
--- proj.android/jni/MailUrlWrapper.cpp
+++ proj.android/jni/MailUrlWrapper.cpp
@@ -1,26 +1,26 @@
 /****************************************************************************
-Copyright (c) 2013-2014 Auticiel SAS
-
-http://www.fennex.org
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************///
+ Copyright (c) 2013-2014 Auticiel SAS
+ 
+ http://www.fennex.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************///
 
 #include <jni.h>
 #include "FenneX.h"
@@ -32,51 +32,54 @@ USING_NS_FENNEX;
 
 void openUrl(const std::string& url)
 {
-	JniMethodInfo minfo;
-	CCAssert(JniHelper::getStaticMethodInfo(minfo, CLASS_NAME, "openUrl", "(Ljava/lang/String;)V"), "Function doesn't exist");
+    JniMethodInfo minfo;
+    bool functionExist = JniHelper::getStaticMethodInfo(minfo, CLASS_NAME, "openUrl", "(Ljava/lang/String;)V");
+    CCAssert(functionExist, "Function doesn't exist");
     jstring jurl = minfo.env->NewStringUTF(url.c_str());
-	minfo.env->CallStaticVoidMethod(minfo.classID, minfo.methodID, jurl);
-	minfo.env->DeleteLocalRef(minfo.classID);
-	minfo.env->DeleteLocalRef(jurl);
+    minfo.env->CallStaticVoidMethod(minfo.classID, minfo.methodID, jurl);
+    minfo.env->DeleteLocalRef(minfo.classID);
+    minfo.env->DeleteLocalRef(jurl);
 }
 
 void sendMail(const std::string& address, const std::string& subject, const std::string& message, const std::string& attachmentPlist)
 {
-	JniMethodInfo minfo;
-	CCAssert(JniHelper::getStaticMethodInfo(minfo, CLASS_NAME, "sendMail", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V"), "Function doesn't exist");
+    JniMethodInfo minfo;
+    bool functionExist = JniHelper::getStaticMethodInfo(minfo, CLASS_NAME, "sendMail", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V");
+    CCAssert(functionExist, "Function doesn't exist");
     jstring jaddress = minfo.env->NewStringUTF(address.c_str());
     jstring jsubject = minfo.env->NewStringUTF(subject.c_str());
     jstring jmessage = minfo.env->NewStringUTF(message.c_str());
-	if(attachmentPlist.empty())
-	{
-		minfo.env->CallStaticVoidMethod(minfo.classID, minfo.methodID, jaddress, jsubject, jmessage);
-	}
-	else
-	{
+    if(attachmentPlist.empty())
+    {
+        minfo.env->CallStaticVoidMethod(minfo.classID, minfo.methodID, jaddress, jsubject, jmessage);
+    }
+    else
+    {
         jstring jattachmentPlist = minfo.env->NewStringUTF(attachmentPlist.c_str());
-		minfo.env->CallStaticVoidMethod(minfo.classID, minfo.methodID, jaddress, jsubject, jmessage, jattachmentPlist);
-	    minfo.env->DeleteLocalRef(jattachmentPlist);
-	}
-	minfo.env->DeleteLocalRef(minfo.classID);
-	minfo.env->DeleteLocalRef(jaddress);
-	minfo.env->DeleteLocalRef(jsubject);
-	minfo.env->DeleteLocalRef(jmessage);
+        minfo.env->CallStaticVoidMethod(minfo.classID, minfo.methodID, jaddress, jsubject, jmessage, jattachmentPlist);
+        minfo.env->DeleteLocalRef(jattachmentPlist);
+    }
+    minfo.env->DeleteLocalRef(minfo.classID);
+    minfo.env->DeleteLocalRef(jaddress);
+    minfo.env->DeleteLocalRef(jsubject);
+    minfo.env->DeleteLocalRef(jmessage);
 }
 
 void sendBackgroundMail(const std::string& from, const std::string& password, const std::string& to, const std::string& subject, const std::string& message)
 {
-	JniMethodInfo minfo;
-	CCAssert(JniHelper::getStaticMethodInfo(minfo, CLASS_NAME, "sendBackgroundMail", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V"), "Function doesn't exist");
+    JniMethodInfo minfo;
+    bool functionExist = JniHelper::getStaticMethodInfo(minfo, CLASS_NAME, "sendBackgroundMail", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V");
+    CCAssert(functionExist, "Function doesn't exist");
     jstring jfrom = minfo.env->NewStringUTF(from.c_str());
     jstring jpassword = minfo.env->NewStringUTF(password.c_str());
     jstring jto = minfo.env->NewStringUTF(to.c_str());
     jstring jsubject = minfo.env->NewStringUTF(subject.c_str());
     jstring jmessage = minfo.env->NewStringUTF(message.c_str());
     minfo.env->CallStaticVoidMethod(minfo.classID, minfo.methodID, jfrom, jpassword, jto, jsubject, jmessage);
-	minfo.env->DeleteLocalRef(minfo.classID);
-	minfo.env->DeleteLocalRef(jfrom);
-	minfo.env->DeleteLocalRef(jpassword);
-	minfo.env->DeleteLocalRef(jto);
-	minfo.env->DeleteLocalRef(jsubject);
-	minfo.env->DeleteLocalRef(jmessage);
+    minfo.env->DeleteLocalRef(minfo.classID);
+    minfo.env->DeleteLocalRef(jfrom);
+    minfo.env->DeleteLocalRef(jpassword);
+    minfo.env->DeleteLocalRef(jto);
+    minfo.env->DeleteLocalRef(jsubject);
+    minfo.env->DeleteLocalRef(jmessage);
 }
