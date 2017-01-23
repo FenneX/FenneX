9f4bff0ad32c820d413a240df6c8a3012a7da3c2
François Dupayrat <fradow@gmail.com>
2017-01-23 17:53:42 +0100

    Remove armeabi from Android compilation, only compile for armeabi-v7a when debugging

--- proj.android/jni/Application.mk
+++ proj.android/jni/Application.mk
@@ -4,13 +4,14 @@ NDK_TOOLCHAIN_VERSION=clang
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=0 -std=c++11 -fsigned-char
 APP_LDFLAGS := -latomic
 
-APP_ABI := armeabi armeabi-v7a x86
 
 APP_DEBUG := $(strip $(NDK_DEBUG))
 ifeq ($(APP_DEBUG),1)
   APP_CPPFLAGS += -DCOCOS2D_DEBUG=1
   APP_OPTIM := debug
+  APP_ABI :=  armeabi-v7a
 else
   APP_CPPFLAGS += -DNDEBUG
   APP_OPTIM := release
+  APP_ABI :=  armeabi-v7a x86
 endif
