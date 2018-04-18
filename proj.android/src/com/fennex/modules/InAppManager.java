825032efa912371940c7947c3e3c114bfc721858
Moowo <pierre.bertinet@gmail.com>
2018-04-18 17:41:35 +0200

    Fix potential crashes when using InAppManager

--- proj.android/src/com/fennex/modules/InAppManager.java
+++ proj.android/src/com/fennex/modules/InAppManager.java
@@ -189,8 +189,8 @@ public class InAppManager implements ActivityResultResponder {
                     mHelper.flagEndAsync();
                 }
                 try {
-                    Purchase purchase = mInventory.getPurchase(productID);
-                    mHelper.launchPurchaseFlow(NativeUtility.getMainActivity(), productID, purchase != null ? purchase.getItemType() : IabHelper.ITEM_TYPE_INAPP, RC_REQUEST,
+                    SkuDetails skuDetails = mInventory.getSkuDetails(productID);
+                    mHelper.launchPurchaseFlow(NativeUtility.getMainActivity(), productID, skuDetails != null ? skuDetails.getType() : IabHelper.ITEM_TYPE_INAPP, RC_REQUEST,
                                 mPurchaseFinishedListener, payload);
                 } catch (IllegalStateException e) {
                     Log.e("InAppManager", "Illegal state exception : " + e.getMessage());
@@ -334,8 +334,9 @@ public class InAppManager implements ActivityResultResponder {
             Log.w(TAG, "mInventory is null, can't get products infos");
             return new String[0];
         }
+
         SkuDetails details = mInventory.getAllSku().get(productId);
-        Log.i("InAppManager", "Price String : " + details.getPrice());
+        Log.i("InAppManager", "Price String : " + details.getPrice() + ", type : " + details.getType());
         NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
         Number price = null;
         try {
@@ -435,7 +436,7 @@ public class InAppManager implements ActivityResultResponder {
                         } else if (result.getResponse() == IabHelper.IABHELPER_VERIFICATION_FAILED) {
                             getInstance().notifyInAppEvent("AuthenticityErrorTransactionFailure", "InvalidPayload", purchase.getToken(), "IABHelper verification failed during purchaseFinished");
                         } else {
-                            getInstance().notifyInAppEvent("ErrorTransactionFailure", "Failure", purchase.getToken(), "Unhandled error during purchaseFinished: " + IABCodeToString(result.getResponse()) + ", code:" + result.getResponse());
+                            getInstance().notifyInAppEvent("ErrorTransactionFailure", "Failure", purchase != null ? purchase.getToken() : "", "Unhandled error during purchaseFinished: " + IABCodeToString(result.getResponse()) + ", code:" + result.getResponse());
                         }
                         return;
                     }
@@ -446,11 +447,11 @@ public class InAppManager implements ActivityResultResponder {
                         return;
                     }
 
-                    if (purchase.getPurchaseState() == 1) //CANCELED
+                    if (purchase != null && purchase.getPurchaseState() == 1) //CANCELED
                     {
                         getInstance().notifyInAppEvent("PayementCanceledTransactionFailure", purchase.getSku(), purchase.getToken(), "Purchase cancelled by developer during purchaseFinished");
                         Log.d(TAG, "Purchase cancelled.");
-                    } else if (purchase.getPurchaseState() == 2) //REFUNDED
+                    } else if (purchase != null && purchase.getPurchaseState() == 2) //REFUNDED
                     {
 
                         getInstance().notifyInAppEvent("ProductRefunded", purchase.getSku(), purchase.getToken(), "Purchase refunded during purchaseFinished");
