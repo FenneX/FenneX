ab69825436ce44abbf09b8a5e2d707f7a4fe2169
François Dupayrat <fradow@gmail.com>
2018-11-16 16:45:36 +0100

    Fix warnings in cocos2d that are fixed upstream

--- cocos2d/cocos/renderer/CCTrianglesCommand.cpp
+++ cocos2d/cocos/renderer/CCTrianglesCommand.cpp
@@ -54,7 +54,7 @@ void TrianglesCommand::init(float globalOrder, GLuint textureID, GLProgramState*
     {
         int count = _triangles.indexCount;
         _triangles.indexCount = count / 3 * 3;
-        CCLOGERROR("Resize indexCount from %zd to %zd, size must be multiple times of 3", count, _triangles.indexCount);
+        CCLOGERROR("Resize indexCount from %d to %d, size must be multiple times of 3", count, _triangles.indexCount);
     }
     _mv = mv;
     
