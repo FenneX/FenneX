35ab3cae9e10cf1c6e8704e1f96a05e2a967164e
Moowo <pierre.bertinet@gmail.com>
2019-03-07 17:28:35 +0100

    Add LongTapRecognizer

---
+++ Classes/FenneX/Core/GesturesHandling/LongTapRecognizer.cpp
@@ -0,0 +1,101 @@
+/****************************************************************************
+ Copyright (c) 2013-2014 Auticiel SAS
+ 
+ http://www.fennex.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************///
+
+#include "LongTapRecognizer.h"
+#include "GraphicLayer.h"
+#include "Shorteners.h"
+#include "AppMacros.h"
+
+#define TIME GraphicLayer::sharedLayer()->getClock()
+
+NS_FENNEX_BEGIN
+// singleton stuff
+static LongTapRecognizer *s_SharedRecognizer = nullptr;
+
+LongTapRecognizer* LongTapRecognizer::sharedRecognizer(void)
+{
+    if (!s_SharedRecognizer)
+    {
+        s_SharedRecognizer = new LongTapRecognizer();
+        s_SharedRecognizer->init();
+    }
+    
+    return s_SharedRecognizer;
+}
+
+void LongTapRecognizer::init()
+{
+    touchStart.clear();
+    touchInitialPosition.clear();
+}
+
+bool LongTapRecognizer::onTouchBegan(Touch *touch, Event *pEvent)
+{
+    touchStart.insert(std::make_pair(touch->getID(), TIME));
+    touchInitialPosition.insert(std::make_pair(touch->getID(), Scene::touchPosition(touch)));
+    return true;
+}
+
+void LongTapRecognizer::onTouchMoved(Touch *touch, Event *pEvent)
+{
+    
+}
+
+void LongTapRecognizer::onTouchEnded(Touch *touch, Event *pEvent)
+{
+#if VERBOSE_TOUCH_RECOGNIZERS
+    log("linked ? %s", mainLinker->linkedObjectOf(touch) == nullptr ? "yes" : "no");
+    log("is in start ? %s", touchStart.find(touch->getID()) != touchStart.end() ? "yes" : "no");
+    log("time ? %s", (TIME - touchStart.at(touch->getID())) >= 2.0 ? "yes" : "no");
+    log("distance ? %s : real : %f", Scene::touchPosition(touch).getDistance(touchInitialPosition.at(touch->getID())) < 20 ? "yes" : "no", Scene::touchPosition(touch).getDistance(touchInitialPosition.at(touch->getID())));
+    log("initial position : %f, %f, current : %f, %f", touchInitialPosition.at(touch->getID()).x, touchInitialPosition.at(touch->getID()).y, Scene::touchPosition(touch).x, Scene::touchPosition(touch).y);
+#endif
+    if(mainLinker != nullptr
+       //&& mainLinker->linkedObjectOf(touch) == nullptr //prevent drag + tap from working at the same time
+       && touchStart.find(touch->getID()) != touchStart.end()
+       && (TIME - touchStart.at(touch->getID())) >= 2.0
+       && Scene::touchPosition(touch).getDistance(touchInitialPosition.at(touch->getID())) < 50 * RESOLUTION_MULTIPLIER)
+    {
+        for(LongTapDelegate* delegate : delegates)
+        {
+            delegate->LongTapRecognized(touch);
+        }
+    }
+    touchStart.erase(touch->getID());
+    touchInitialPosition.erase(touch->getID());
+}
+
+void LongTapRecognizer::cleanTouches()
+{
+    touchStart.clear();
+    touchInitialPosition.clear();
+}
+
+void LongTapRecognizer::cancelRecognitionForTouch(Touch* touch)
+{
+    touchStart.erase(touch->getID());
+    touchInitialPosition.erase(touch->getID());
+}
+
+NS_FENNEX_END
---
+++ Classes/FenneX/Core/GesturesHandling/LongTapRecognizer.h
@@ -0,0 +1,64 @@
+/****************************************************************************
+ Copyright (c) 2013-2014 Auticiel SAS
+ 
+ http://www.fennex.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************///
+
+
+#ifndef __FenneX__LongTapRecognizer__
+#define __FenneX__LongTapRecognizer__
+
+#include "cocos2d.h"
+USING_NS_CC;
+#include "DelegatingRecognizer.h"
+#include "FenneXMacros.h"
+
+NS_FENNEX_BEGIN
+
+class LongTapDelegate
+{
+public:
+    virtual void LongTapRecognized(Touch* touch) = 0;
+};
+
+//Doesn't need to be updated
+// You need to subscribe to the recognizer by adding a delegate (it'll launch the LongTapRecognized method)
+// A Long tap is longer than 2s
+class LongTapRecognizer : public DelegatingRecognizer<LongTapDelegate>
+{
+public:
+    static LongTapRecognizer* sharedRecognizer(void);
+    
+    virtual bool onTouchBegan(Touch *touch, Event *pEvent);
+    virtual void onTouchMoved(Touch *touch, Event *pEvent);
+    virtual void onTouchEnded(Touch *touch, Event *pEvent);
+    virtual void cleanTouches();
+    void cancelRecognitionForTouch(Touch* touch);
+protected:
+    void init();
+    
+protected:
+    std::map<int, float> touchStart;
+    std::map<int, Vec2> touchInitialPosition;
+};
+NS_FENNEX_END
+
+#endif /* defined(__FenneX__LongTapRecognizer__) */
